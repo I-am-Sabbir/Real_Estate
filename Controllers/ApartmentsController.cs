e8d0172841fdab2b9f584708de361b936dbf2f7e
Sabbir <45666033+I-am-Sabbir@users.noreply.github.com>
2020-04-07 22:53:12 +0600

    Controllers&View

---
+++ Controllers/ApartmentsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Real_Estate.Models;
+
+namespace Real_Estate.Controllers
+{
+    public class ApartmentsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ApartmentsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Apartments
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Apartments.Include(a => a.Builder);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Apartments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var apartment = await _context.Apartments
+                .Include(a => a.Builder)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (apartment == null)
+            {
+                return NotFound();
+            }
+
+            return View(apartment);
+        }
+
+        // GET: Apartments/Create
+        public IActionResult Create()
+        {
+            ViewData["BuilderID"] = new SelectList(_context.Builders, "UserID", "Company_Name");
+            return View();
+        }
+
+        // POST: Apartments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Apartment_Name,Location,Size,Details,BuilderID")] Apartment apartment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(apartment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BuilderID"] = new SelectList(_context.Builders, "UserID", "Company_Name", apartment.BuilderID);
+            return View(apartment);
+        }
+
+        // GET: Apartments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var apartment = await _context.Apartments.FindAsync(id);
+            if (apartment == null)
+            {
+                return NotFound();
+            }
+            ViewData["BuilderID"] = new SelectList(_context.Builders, "UserID", "Company_Name", apartment.BuilderID);
+            return View(apartment);
+        }
+
+        // POST: Apartments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Apartment_Name,Location,Size,Details,BuilderID")] Apartment apartment)
+        {
+            if (id != apartment.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(apartment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ApartmentExists(apartment.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BuilderID"] = new SelectList(_context.Builders, "UserID", "Company_Name", apartment.BuilderID);
+            return View(apartment);
+        }
+
+        // GET: Apartments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var apartment = await _context.Apartments
+                .Include(a => a.Builder)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (apartment == null)
+            {
+                return NotFound();
+            }
+
+            return View(apartment);
+        }
+
+        // POST: Apartments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var apartment = await _context.Apartments.FindAsync(id);
+            _context.Apartments.Remove(apartment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ApartmentExists(int id)
+        {
+            return _context.Apartments.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Controllers/RequestedsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Real_Estate.Models;
+
+namespace Real_Estate.Controllers
+{
+    public class RequestedsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public RequestedsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Requesteds
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Requesteds.ToListAsync());
+        }
+
+        // GET: Requesteds/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var requested = await _context.Requesteds
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (requested == null)
+            {
+                return NotFound();
+            }
+
+            return View(requested);
+        }
+
+        // GET: Requesteds/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Requesteds/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID")] Requested requested)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(requested);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(requested);
+        }
+
+        // GET: Requesteds/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var requested = await _context.Requesteds.FindAsync(id);
+            if (requested == null)
+            {
+                return NotFound();
+            }
+            return View(requested);
+        }
+
+        // POST: Requesteds/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID")] Requested requested)
+        {
+            if (id != requested.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(requested);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RequestedExists(requested.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(requested);
+        }
+
+        // GET: Requesteds/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var requested = await _context.Requesteds
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (requested == null)
+            {
+                return NotFound();
+            }
+
+            return View(requested);
+        }
+
+        // POST: Requesteds/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var requested = await _context.Requesteds.FindAsync(id);
+            _context.Requesteds.Remove(requested);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RequestedExists(int id)
+        {
+            return _context.Requesteds.Any(e => e.ID == id);
+        }
+    }
+}
